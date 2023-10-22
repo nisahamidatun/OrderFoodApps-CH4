cd0b3a94b5bd51595651d139b0408545c7c1dca2
Hamidatun Nisa <hamidatunnisa9d@gmail.com>
2023-10-22 21:45:01 +0700

    Finished feature cart api ch5

--- app/src/main/java/com/learning/orderfoodappsch3/model/Cart.kt
+++ app/src/main/java/com/learning/orderfoodappsch3/model/Cart.kt
@@ -1,8 +1,11 @@
 package com.learning.orderfoodappsch3.model
 
-data class Cart (
-    var id: Int = 0,
+data class Cart(
+    var id: Int ? = null,
     var orderfoodId: Int = 0,
+    val orderfoodName: String,
+    val orderfoodPrice: Int,
+    val orderfoodImgUrl: String,
     var quantityCartItem: Int = 0,
-    var notes: String? = null
+    var notes: String? = null,
 )
